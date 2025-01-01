1240bce95b732958ee6cb89689377c3fadc4195c
dwigapra <115785987+dwigapra@users.noreply.github.com>
2025-01-01 18:03:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import streamlit as st
 import runpy
 
 def main():
-    st.title("Main Menu")
-
     # Main menu options
     menu = ["SVM", "Random Forest"]
     choice = st.sidebar.selectbox("Select Main Menu", menu)
@@ -14,31 +12,27 @@ def main():
         rf_sub_menu()
 
 def svm_sub_menu():
-    st.subheader("SVM Menu")
+    st.subheader("SVM")
 
     # SVM submenu options
     sub_menu = ["Fish", "Fruit"]
     choice = st.sidebar.radio("Select Sub Menu", sub_menu)
 
     if choice == "Fish":
-        st.write("You selected SVM -> Fish.")
         # Add SVM Fish specific content or functionality here
     elif choice == "Fruit":
-        st.write("You selected SVM -> Fruit.")
         # Add SVM Fruit specific content or functionality here
 
 def rf_sub_menu():
-    st.subheader("Random Forest Menu")
+    st.subheader("Random Forest")
 
     # Random Forest submenu options
     sub_menu = ["Fish", "Fruit"]
     choice = st.sidebar.radio("Select Sub Menu", sub_menu)
 
     if choice == "Fish":
-        st.write("You selected Random Forest -> Fish.")
         runpy.run_path('RF/app.py')
     elif choice == "Fruit":
-        st.write("You selected Random Forest -> Fruit.")
         # Add Random Forest Fruit specific content or functionality here
 
 if __name__ == "__main__":
