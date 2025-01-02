6d89b918fc4cfe8783896895d4e11c36b97c4e17
dwigapra <115785987+dwigapra@users.noreply.github.com>
2025-01-02 16:03:30 +0700

    Update app.py

--- SVM/Fish/app.py
+++ SVM/Fish/app.py
@@ -1,20 +1,18 @@
 import streamlit as st
 import pickle
 import pandas as pd
+import os
 
 # Load the Naive Bayes model
-try:
-    with open('svm_model_fish.pkl', 'rb') as model_file:
-        svm_model = pickle.load(model_file)
-    
-    with open('svm_scaler_fish.pkl', 'rb') as scaler_file:
-        scaler = pickle.load(scaler_file)
-        
-    model_loaded = True
-except FileNotFoundError:
-    st.error("File model atau scaler tidak ditemukan. Harap periksa path file.")
-    model_loaded = False
+current_dir = os.path.dirname(__file__)
+model_path = os.path.join(current_dir, 'svm_model_fish.pkl')
+scaler_path = os.path.join(current_dir, 'svm_scaler_fish.pkl')
 
+with open(model_path, 'rb') as model_file:
+    svm_model = pickle.load(model_file)
+
+with open(scaler_path, 'rb') as scaler_file:
+    scaler = pickle.load(model_file)
 
 # Judul aplikasi
 st.title("Aplikasi Klasifikasi Jenis Ikan")
@@ -27,15 +25,14 @@ w_l_ratio = st.number_input("Masukkan rasio lebar-panjang ikan:", min_value=0.0,
 
 # Prediksi
 if st.button("Prediksi"):
-    if model_loaded:
-        # Data input dari pengguna
-        input_data = pd.DataFrame([[length, weight, w_l_ratio]], columns=['length', 'weight', 'w_l_ratio'])
+    # Data input dari pengguna
+    input_data = pd.DataFrame([[length, weight, w_l_ratio]], columns=['length', 'weight', 'w_l_ratio'])
         
-        # Scale data baru sebelum prediksi
-        input_data_scaled = scaler.transform(input_data)
-        
-        # Prediksi menggunakan model
-        prediction = svm_model.predict(input_data_scaled)
-        st.write(f"Spesies Ikan: {prediction[0]}")
-    else:
-        st.error("Model atau scaler tidak berhasil dimuat.")
+    # Scale data baru sebelum prediksi
+    input_data_scaled = scaler.transform(input_data)
+    
+    # Prediksi menggunakan model
+    prediction = svm_model.predict(input_data_scaled)
+    st.write(f"Spesies Ikan: {prediction[0]}")
+else:
+    st.error("Model atau scaler tidak berhasil dimuat.")
